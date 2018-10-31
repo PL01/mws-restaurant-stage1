8acda8f39833726184c7324de1dfcb5013f40850
Pablo Lara <pablolara01@yahoo.com>
2018-10-30 20:45:47 -0400

    Uploading sw.js

--- sw.js
+++ sw.js
@@ -21,34 +21,34 @@ const cacheFiles = [
     '/img/10.jpg'
 ];
 
-self.addEventListener('install', function(e){
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('v1').then(function(cache){
+        caches.open('v1').then(function (cache) {
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-self.addEventListener('fetch', function(e){
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(response){
+        caches.match(e.request).then(function (response) {
             if (response) {
                 console.log('Found ', e.request, ' in cache');
                 return response;
             }
-            else{
+            else {
                 console.log('Could not find ', e.request, ' in cache, FETCHING!');
                 return fetch(e.request)
-                .then(function (response) {
-                    const clonedResponse = response.clone();
-                    caches.open('v1').then(function (cache) {
-                        cache.put(e.request, clonedResponse);
+                    .then(function (response) {
+                        const clonedResponse = response.clone();
+                        caches.open('v1').then(function (cache) {
+                            cache.put(e.request, clonedResponse);
+                        })
+                        return response;
                     })
-                    return response;
-                })
-                .catch(function (err) {
-                    console.error(err);
-                });
+                    .catch(function (err) {
+                        console.error(err);
+                    });
             }
         })
     );
